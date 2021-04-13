7d44e1d8af9c5f23ece43418f427bf551fee7ba5
flyash <fenxuehaiyan@qq.com>
2021-04-13 17:24:27 +0800

    use v prefix

--- package.sh
+++ package.sh
@@ -41,7 +41,7 @@ if [[ $version == "latest" ]]
 then
   wget https://github.com/felix-fly/xray-openwrt/releases/latest/download/xray-linux-$platform.tar.gz -O xray.tar.gz
 else
-  wget https://github.com/felix-fly/xray-openwrt/releases/download/$version/xray-linux-$platform.tar.gz -O xray.tar.gz
+  wget https://github.com/felix-fly/xray-openwrt/releases/download/v$version/xray-linux-$platform.tar.gz -O xray.tar.gz
 fi
 
 tar -xzvf xray.tar.gz
